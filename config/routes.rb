89044df1223dab215d9f9843e4b0305903d43342
Gordana Marjanovic <gogamar@gmail.com>
2024-03-16 16:39:34 +0100

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'pages/home'
+  get 'pages/about'
+  get 'pages/contact'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root to: "pages#home"
+end
